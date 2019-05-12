4a84b9020208e90aa47c508fdffb5592283af1a6
Mateo Oviedo Torres <mateooviedotorres@gmail.com>
2019-05-11 21:02:19 -0500

    Se agrega import de territorio model en subdito model para arreglar la relacion

--- application_got/subdito/models.py
+++ application_got/subdito/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+# Models
+from territorio.models import Territorio
+
 # Create your models here.
 class Subdito(models.Model):
     nombre_subdito = models.CharField(max_length=30)
     aflicion = models.CharField(max_length=100)
-    id_territorio = models.ForeignKey('Territorio', models.DO_NOTHING, db_column='id_territorio')
+    id_territorio = models.ForeignKey(Territorio, models.DO_NOTHING, db_column='id_territorio')
 
     class Meta:
         managed = False
